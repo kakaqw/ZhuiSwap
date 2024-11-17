814b52633d291284c80192344a1abe04e1624224
0xkaka <zhuhaojun64@gmail.com>
2024-11-17 14:43:10 +0800

    没养成写一步测试一下的习惯现在写测试有点无从下手了，打算直接做页面部署测试

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -2,9 +2,14 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
+import {ZhuiswapPair} from "../src/Zhuiswap-Pair.sol";
 
 // import {Counter} from "../src/Counter.sol";
 
 contract CounterTest is Test {
+    function setUp() public {
+        zhuiswapPair = new ZhuiswapPair();
+    }
 
+    function testMint() public {}
 }
